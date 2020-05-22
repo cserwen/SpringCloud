4684129b4df35ca17832d1d4d841b7f03df131cb
dengzw <litterboydeng@qq.com>
2020-05-23 00:11:29 +0800

    创建并实现producer/consumer 两个demo

--- producer/src/main/java/com/example/producer/ProducerApplication.java
+++ producer/src/main/java/com/example/producer/ProducerApplication.java
@@ -1,17 +1,15 @@
-package com.example.netflixzuul;
+package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
-public class NetflixZuulApplication {
+public class ProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NetflixZuulApplication.class, args);
+        SpringApplication.run(ProducerApplication.class, args);
     }
 
 }
---
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -0,0 +1,15 @@
+package com.example.producer;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProducerController {
+
+    @RequestMapping("/producer/demo")
+    public String produce(){
+        return "Hello !This is from producer!";
+    }
+}
