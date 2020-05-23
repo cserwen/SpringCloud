46a9038f7c52706fa2b742e9703be62d83ec0f2f
dengzw <litterboydeng@qq.com>
2020-05-23 10:07:16 +0800

    创建并实现producer/consumer 两个demo

--- producer/src/main/java/com/example/producer/ProducerController.java
+++ producer/src/main/java/com/example/producer/ProducerController.java
@@ -2,14 +2,13 @@ package com.example.producer;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProducerController {
 
     @RequestMapping("/producer/demo")
-    public String produce(){
+    public String produce(){    
         return "Hello !This is from producer!";
     }
 }
